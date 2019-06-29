4d3b079f54832c39a7212bed239df89df39485aa
kcoffland <kyle.coffland@gmail.com>
2019-06-29 10:23:51 -0500

    Added the ability to set the radius. Added functionality to stop selenium from crashing when running at scale. Made code more modular

--- data_acquisition/IndeedScraper.py
+++ data_acquisition/IndeedScraper.py
@@ -1,50 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException
 from bs4 import BeautifulSoup
 import os
 import shutil
 import csv
 
 base_url = 'https://www.indeed.com'
+driver_path = '/home/kyle/Downloads/chromedriver'
 
 
 class IndeedScraper:
 
-    def __init__(self, job_title, key_words, location='', starting_page=1, radius=25):
+    def __init__(self, job_title, key_words, location='', starting_page=1, radius='Exact location only'):
         """
         Args:
              job_title: string, title used when searching for jobs on indeed.com
              key_words: list of strings, key words that should be in the job titles to help remove ads
              location: string, location used when searching for jobs on indeed.com
              starting_page: int, page number which the user wants to start on
-             radius: int, maximum distance in miles from given location that a job can be
+             radius: str, maximum distance in miles from given location that a job can be
+                                the string 'Exact location only' is the default value,
+                                and other values can only be 5, 10, 15, 25, 50, 100
         """
 
         self.job_title = job_title
         self.location = location
         self.page_number = starting_page
         self.key_words = key_words
-        self.radius = radius
+        try:
+            self.radius = int(radius)
+        except ValueError:
+            # Only executes if default value is given
+            self.radius = radius
 
         try:
-            self.driver = webdriver.Chrome(executable_path='/home/kyle/Downloads/chromedriver')
-            self.driver.get(base_url)
+            self.driver = webdriver.Chrome(executable_path=driver_path)
+            self.load_page(base_url)
         except WebDriverException:
             raise RuntimeError("Driver Unable to connect")
 
         # Starting at the correct page immediately
         if self.page_number > 1:
             # Formatting the title and url for the website
-            f_title = self.job_title.replace(' ', '+')
-            f_location = self.location.replace(' ', '+')
-            f_location = f_location.replace(',', '%2C')
-
-            if self.location:
-                self.driver.get(base_url + f'/jobs?q={f_title}&l={f_location}&start={(self.page_number-1)*10}')
-            else:
-                self.driver.get(base_url + f'/jobs?q={f_title}&start={(self.page_number-1)*10}')
+            self.load_page()
 
     def search_jobs(self):
 
@@ -64,26 +64,37 @@ class IndeedScraper:
         find_jobs_button = self.driver.find_element_by_class_name('icl-Button--primary')
         find_jobs_button.click()
 
+        self.driver.minimize_window()
+
         # Setting the radius to the given distance
         if self.location:
             select_radius = Select(self.driver.find_element_by_id('distance_selector'))
-            select_radius.select_by_visible_text(f'within {self.radius} miles')
+            if type(self.radius) == int:
+                select_radius.select_by_visible_text(f'within {self.radius} miles')
+            elif type(self.radius) == str:
+                select_radius.select_by_visible_text(self.radius)
 
     def next_page(self):
         # Attempts to call the next page
         try:
-            next_button = self.driver.find_element_by_partial_link_text('Next')
+            # Ensuring this is the correct page
+            current_url = self.driver.current_url
+            if current_url not in base_url+self.get_url():
+                self.load_page()
+
+            # Following the next_button link
+            next_button = self.driver.find_element_by_link_text('Next »')
             next_html = next_button.get_attribute('outerHTML')
             soup = BeautifulSoup(next_html, 'html.parser')
-            url = soup.a['href']
-            self.driver.get(base_url+url)
+            next_url = soup.a['href']
+            self.load_page(base_url+next_url)
             return True
         except NoSuchElementException:
             return False
 
     def get_data(self):
         # Restarting the driver periodically ensures the browser doesn't crash
-        if self.page_number % 20 == 0:
+        if self.page_number % 10 == 0:
             self.restart_driver()
 
         links = self.driver.find_elements_by_class_name('title')
@@ -97,12 +108,19 @@ class IndeedScraper:
             # on any hyperlinked text
             try:
                 links = self.driver.find_elements_by_class_name('title')
+                # If the list is empty, there was an issue getting back to the postings page
+                if not links:
+                    self.load_page()
+                    links = self.driver.find_elements_by_class_name('title')
             except WebDriverException:
                 # Only happens when the driver has crashed and needs to be restarted
                 self.restart_driver()
                 links = self.driver.find_elements_by_class_name('title')
 
-            soup = BeautifulSoup(links[i].get_attribute('outerHTML'), 'html.parser')
+            try:
+                soup = BeautifulSoup(links[i].get_attribute('outerHTML'), 'html.parser')
+            except IndexError:
+                continue
 
             posting_title = soup.text.rstrip().lstrip().lower()
             # Moving onto the next link if the title doesn't contain an appropriate key word
@@ -111,7 +129,15 @@ class IndeedScraper:
             posting_data['Title'] = posting_title
 
             url = soup.a['href']
-            self.driver.get(base_url+url)
+            self.load_page(base_url+url)
+
+            if base_url not in self.driver.current_url:
+                # If this executes, the driver has left indeed due to following some recommended
+                # job postings. This also indicates the end of the page, so I can just return
+                # all of the data collected
+                self.page_number += 1
+                self.driver.execute_script("window.history.go(-1)")
+                return page_data
 
             # Getting the rest of the data from each Job posting
             posting_soup = BeautifulSoup(self.driver.page_source, 'html.parser')
@@ -166,28 +192,65 @@ class IndeedScraper:
         try:
             self.close()
         except WebDriverException:
+            # The window has already closed due to some other reason, so I can move on
             pass
 
-        self.driver = webdriver.Chrome(executable_path='/home/kyle/Downloads/chromedriver')
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.driver.minimize_window()
+        self.load_page()
 
+    def get_url(self):
+        """Creates the url for the indeed website from the class attributes
+        Returns:
+            url: string, the formatted url for the website
+        """
         # Formatting the title and url for the website
         f_title = self.job_title.replace(' ', '+')
         f_location = self.location.replace(' ', '+')
         f_location = f_location.replace(',', '%2C')
+        f_page_number = (self.page_number - 1) * 10
+        f_radius = self.radius
+        if type(self.radius) == str:
+            f_radius = 0
 
         if self.location:
-            self.driver.get(base_url + f'/jobs?q={f_title}&l={f_location}&start={(self.page_number - 1) * 10}')
+            url = f'/jobs?q={f_title}&l={f_location}&' \
+                f'radius={f_radius}&start={f_page_number}'
         else:
-            self.driver.get(base_url + f'/jobs?q={f_title}&start={(self.page_number - 1) * 10}')
+            url = f'/jobs?q={f_title}&radius={self.radius}&' \
+                f'start={f_page_number}'
+
+        return url
 
     def close(self):
         self.driver.close()
 
+    def load_page(self, url='default'):
+        """Loads the page of the given url
+        Args:
+            url: string, url which needs to be loaded. If no url is given, then the url
+                        will be created from the class attributes and base_url
+        """
+        if url == 'default':
+            url = base_url + self.get_url()
+        count = 0
+        # Loads page, but if it fails, then it will try again
+        while count < 10:
+            try:
+                self.driver.refresh()
+                self.driver.get(url)
+                return
+            except TimeoutException:
+                pass
+            count += 1
+
+        self.restart_driver()
+
 
 if __name__ == '__main__':
-    starting_page = 1
+    starting_page = 19
     key_words = ['scientist', 'research', 'engineer', 'analyst', 'data']
-    s = IndeedScraper('Data Scientist', key_words=key_words, starting_page=starting_page, location='Chicago', radius=10)
+    s = IndeedScraper('Data Scientist', key_words=key_words, starting_page=starting_page, location='Chicago')
 
     count = starting_page
 
